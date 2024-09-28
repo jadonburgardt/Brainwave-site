71845ba22f6dedae624c2883f7d07755a57f3c92
jadonburgardt <burgardtjad@gmail.com>
2024-09-28 15:19:57 -0500

    more progress on hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,11 +1,11 @@
-import { curve } from "../assets";
+import { curve, heroBackground, robot } from "../assets";
 import Section from "./Section";
 import Button from "./Button";
 
 const Hero = () => {
   return (
     <Section
-      className="pt-[12rem] -mt-[5.25]"
+      className="pt-[12rem] -mt-[5.25rem]"
       crosses
       crossesOffset="lg:translate-y-[5.25rem]"
       customPaddings
@@ -14,7 +14,7 @@ const Hero = () => {
       <div className="container relative">
         <div className="relative z-1 max-w-[62rem] mx-auto text-center mb-[4rem] md:mb-20 lg:mb:[6rem] ">
           <h1 className="h1 mb-6">
-            Explore the Possibilities of AI Chatting with BrainwaveCurve
+            Explore the Possibilities of AI Chatting with Brainwave
             <span className="inline-block relative">
               Brainwave{" "}
               <img
@@ -33,6 +33,31 @@ const Hero = () => {
           <Button href="/pricing" white>
             Get Started
           </Button>
+        </div>
+        <div className="relative max-w-[23rem] mx-auto md:max-w-5xl xl:mb-24">
+          <div className="relative z-1 p-0.5 rounded-2xl bg-conic-gradient">
+            <div className="relative bg-n-8 rounded-[1rem]">
+              <div className="h-[1.4rem] bg-n-10 rounded-t-[0.9rem]" />
+              <div className="aspect-[33/40] rounded-b-[0.9rem] overflow-hidden md:aspect-[688/490] lg:aspect-[1024/490]">
+                <img
+                  src={robot}
+                  className="w-full scale-[1.7] translate-y-[8%] md:scale-[1] md:-translate-y-[10%] lg:-translate-y-[23%]"
+                  width={1024}
+                  height={490}
+                  alt="AI"
+                />
+              </div>
+            </div>
+          </div>
+          <div className="absolute -top-[54%] left-1/2 w-[234%] -translate-x-1/2 md:-top-[46%] md:w-[138%] lg:-top-[104%]">
+            <img
+              src={heroBackground}
+              className="w-full"
+              width={1440}
+              height={1800}
+              alt="hero"
+            />
+          </div>
         </div>
       </div>
     </Section>
