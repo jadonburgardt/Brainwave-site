2d0a5f1e2096ca97ff3913c247d8118ab455e668
jadonburgardt <burgardtjad@gmail.com>
2024-09-28 22:46:23 -0500

    services section done

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
 import ButtonGradient from "./assets/svg/ButtonGradient";
 import Benefits from "./components/Benefits";
+import Collaboration from "./components/Collaboration";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
+import Services from "./components/Services";
 
 const App = () => {
   return (
@@ -10,6 +12,8 @@ const App = () => {
         <Header />
         <Hero />
         <Benefits />
+        <Collaboration />
+        <Services />
       </div>
       <ButtonGradient />
     </>
